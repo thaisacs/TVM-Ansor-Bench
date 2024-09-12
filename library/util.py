176226cbaadb3fac8b1b345694b2c9bf406acd35
thaisacs <thaiscamachoo@gmail.com>
2024-09-11 21:49:34 -0300

    up space

--- library/util.py
+++ library/util.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 
 network_to_n_trials = {
-    #"resnet_50": 22000,
-    #"mobilenet_v2": 16000,
-    #"inception_v3": 26000,
-    #"bert": 12000,
-    "resnet_50": 10000,
-    "resnet_18": 10000,
+    "resnet_50": 22000,
+    "resnet_18": 18000,
     "mobilenet_v2": 10000,
     "mobilenet_v3": 10000,
     "wide_resnet_50": 10000,
     "resnext_50": 10000,
     "resnet3d_18": 10000,
     "inception_v3": 10000,
-    "densenet_121": 66000,
+    "densenet_121": 72000,
     "vgg_16": 10000,
     "bert": 10000,
     "dcgan": 10000,
+    #"mobilenet_v2": 16000,
+    #"inception_v3": 26000,
+    #"bert": 12000,
 }
 
 networks_dict = {
