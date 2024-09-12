34472e1ada0dea5339a799e2254477d9dfba4315
thais <thaiscamachoo@gmail.com>
2024-09-11 23:27:28 -0300

    Adding search space size.

--- library/util.py
+++ library/util.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
 network_to_n_trials = {
-    "resnet_50": 22000,
+    "resnet_50": 27000,
     "resnet_18": 18000,
     "mobilenet_v2": 32000,
-    "mobilenet_v3": 10000,
-    "wide_resnet_50": 10000,
-    "resnext_50": 10000,
-    "resnet3d_18": 10000,
-    "inception_v3": 10000,
+    "mobilenet_v3": 52000,
+    "wide_resnet_50": 27000,
+    "resnext_50": 27000,
+    "resnet3d_18": 17000,
+    "inception_v3": 55000,
     "densenet_121": 72000,
-    "vgg_16": 10000,
-    "bert": 10000,
-    "dcgan": 10000,
+    "bert": 9000,
+    "dcgan": 5000,
 }
 
 networks_dict = {
@@ -25,7 +24,6 @@ networks_dict = {
     "resnet3d_18",
     "inception_v3",
     "densenet_121",
-    "vgg_16",
     "bert",
     "dcgan"
 }
