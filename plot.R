39f043cd4e6fb321ecae38ceb0cef3fcb0aaaf07
thais <thaiscamachoo@gmail.com>
2024-10-25 00:28:00 -0300

    up

---
+++ plot.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(reshape2)
+library(ggstatsplot)
+library(tcltk)
+library(rstatix)
+library(PMCMRplus)
+
+data <- read.csv(file = '/home/thais/Dev/TVMBench/dataset.csv', sep = ',', header = T)
+
+ggplot(data=data, aes(x=iteration, y=value, group=tipo)) +
+  geom_errorbar(aes(ymin=value-desvio, ymax=value+desvio, color=tipo), width=0.2, size=.3) +
+  geom_line(aes(color=tipo), size=1) +
+  coord_cartesian(ylim=c(1,2), xlim=c(1,300))+
+  geom_vline(xintercept=0, linetype="dashed", size=1)+
+  labs(color = element_blank(), x="Iteração", y="Média do Melhor Custo Normalizado")+
+  theme(legend.justification=c(1, 1),legend.position=c(.9, .9),legend.title=element_blank())+
+  #theme(panel.background = element_rect(fill = 'white', colour = 'gray'),
+  #      panel.grid.major = element_line(color = 'light gray'),
+  #      panel.grid.minor = element_line(color = 'light gray'),
+  #      axis.title.y=element_blank(),
+  #      axis.text.y = element_text(size=8),
+  #      legend.background = element_rect(fill=alpha('white', 0.6)))+
+  ggtitle('a) Melhor Custo Encontrado')+
+  scale_x_continuous(breaks = seq(0, 1000, 50))
